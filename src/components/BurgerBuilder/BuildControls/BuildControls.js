cc12d9543706f9db63bcccc84cc0b5a78d81d878
Abdelaziz <abdelazizMalfy@gmail.com>
2019-06-26 17:42:24 +0200

    Burger Controls Added

---
+++ src/components/BurgerBuilder/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type:'salad'},
+    { label: 'Bacon', type:'bacon'},
+    { label: 'Cheese', type:'cheese'},
+    { label: 'Meat', type:'meat'},
+];
+
+function BuildControls(props) {
+    return (
+        <div className="BuildControls">
+            {controls.map(ctrl =>(
+                <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                type={ctrl.type} />
+            ))}
+        </div>
+    )
+}
+
+export default BuildControls;
--- src/components/BurgerBuilder/Burger/Burger.js
+++ src/components/BurgerBuilder/Burger/Burger.js
@@ -4,9 +4,24 @@ import './Burger.css'
 import BurgerIng from './BurgerIng/BurgerIng';
 
 function Burger(props) {
+    let ingredientsArr = Object.keys(props.ingredients).map((igKey) => {
+            return [...Array(props.ingredients[igKey] )].map((_,i)=>{
+               return <BurgerIng key={igKey+i} type={igKey} />
+            })
+        })
+        .reduce((arr,el) =>{
+            return arr.concat(el)
+        },[]);
+    
+    if ( ingredientsArr.length === 0){
+        ingredientsArr = <p>Please Start Adding ingredients!</p>;
+    }
+
     return (
         <div className="Burger">
             <BurgerIng type="bread-top" />
+            {ingredientsArr}
+            <BurgerIng type="bread-bottom" />
         </div>
     )
 }

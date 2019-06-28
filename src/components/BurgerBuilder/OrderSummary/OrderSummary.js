6c2c93927453c128ec2d81aba02eb5479fd355bb
Abdelaziz <abdelazizMalfy@gmail.com>
2019-06-28 23:44:30 +0200

    order summary done

--- src/components/BurgerBuilder/OrderSummary/OrderSummary.js
+++ src/components/BurgerBuilder/OrderSummary/OrderSummary.js
@@ -18,7 +18,7 @@ function OrderSummary(props) {
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)} </strong></p>
+            <p><strong>Total Price: {props.price.toFixed(2)} $</strong></p>
             <p>Continue to Checkout ?</p>
             <Button clicked={props.purchaseCancelled} btnType="Danger">
                 CANCEL

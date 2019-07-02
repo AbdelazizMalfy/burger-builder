20f671636bd836a74fb8be6f3e582faaa0a8043c
Abdelaziz <abdelazizMalfy@gmail.com>
2019-07-02 16:17:14 +0200

    Spinner Added

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from './Burger/Burger';
 import BuildControls from './BuildControls/BuildControls';
 import Modal from '../UI/Modal/Modal';
 import OrderSummary from './OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 import axios from  '../../axios-orders';
 
@@ -24,7 +25,8 @@ class BurgerBuilder extends Component {
              meat:0
          },
          totalPrice: 4,
-         purchasing: false
+         purchasing: false,
+         loading: false
     }
 
     
@@ -78,6 +80,7 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
+        this.setState({loading:true})
         const order = {
             ingredients:this.state.ingredients,
             price:this.state.totalPrice,
@@ -93,19 +96,23 @@ class BurgerBuilder extends Component {
             deliveryMethod: "fastest"
         }
         axios.post('/orders.json', order)
-            .then( response => console.log(response))
-            .catch( error => console.log(error));
+            .then( response => {
+                this.setState({loading:false, purchasing: false })
+            })
+            .catch( error => {
+                this.setState({loading:false, purchasing: false })
+            });
     }
     
     render() {
         return (
             <div>
                 { this.state.purchasing ? <Modal modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
+                    {!this.state.loading ? <OrderSummary 
                     purchaseCancelled={this.purchaseCancelHandler}
                     purchaseContinued={this.purchaseContinueHandler}
                     price={this.state.totalPrice}
-                    ingredients={this.state.ingredients} />
+                    ingredients={this.state.ingredients} /> : <Spinner/>}
                 </Modal> : null}
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 

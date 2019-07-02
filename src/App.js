608e25360b09a8126b682ca245141ee9bc6f660f
Abdelaziz <abdelazizMalfy@gmail.com>
2019-07-02 19:36:30 +0200

    Checkout Summary added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './components/BurgerBuilder/BurgerBuilder';
+import Checkout from './components/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     </div>
   );

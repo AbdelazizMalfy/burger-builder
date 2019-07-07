cbeb6679e09587f2082ab80f268f05c780a152e9
Abdelaziz <abdelazizMalfy@gmail.com>
2019-07-07 13:22:04 +0200

    Validation Added

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,11 +3,16 @@ import './Input.css'
 ;
 function Input(props) {
     let inputElement = null;
+    let inputClasses= ['InputElement']
+
+    if (props.invalid && props.shouldValidate && props.touched)  {
+        inputClasses.push('Invalid')
+    }
 
 
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input className="InputElement" {...props.elementConfig} value={props.value} onChange={props.changed}/>;
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ('textarea'):
             inputElement = <textarea className="InputElement" {...props.elementConfig} value={props.value} onChange={props.changed}/>;

611a280b05314f0be70bde1a3c387885513a4f37
Abdelaziz <abdelazizMalfy@gmail.com>
2019-07-07 18:55:38 +0200

    ready for deployment

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import LogoImage from '../../assets/images/133 burger-logo.png';
 import './Logo.css';
  
-function Logo(props) {
-    return (
-        <div className="Logo" style={{height:props.height}}>
-            <img src={LogoImage} alt="MyBurger" />
-        </div>
-    )
+
+class Logo extends Component {
+
+    // onLogoClicked = () =>{
+    //     this.props.history.push('/');
+    // }
+
+    render(){
+        return (
+            <div className="Logo"  style={{height:this.props.height}}>
+               <Link to='/'><img  src={LogoImage} alt="MyBurger" /></Link>
+            </div>
+        )
+    }
 }
 
 export default Logo;
